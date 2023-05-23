0ec0d1c2e496ac7d6eac79b41f982be5a95f52d3
aiden <aiden@fanding.kr>
2023-05-23 17:16:52 +0900

    add: init project

--- SwiftSimplePlayer/ViewController.swift
+++ SwiftSimplePlayer/ViewController.swift
@@ -6,14 +6,72 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVPlayer?
+    var audioPlayer: AVAudioPlayer!
+    var playerViewController: AVPlayerViewController?
+    var playerLayer: AVPlayerLayer?
 
+    @IBAction func btnPlayFullScreen(_ sender: UIButton) {
+        playVideoFullScreen()
+    }
+    @IBAction func btnPlay(_ sender: UIButton) {
+        player?.play()
+    }
+    @IBAction func btnPause(_ sender: UIButton) {
+        player?.pause()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupAVAudioSession()
+        setupVideo()
+        
     }
-
-
+    
+    private func setupVideo() {
+//        let url = NSURL(string: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")!
+        let url = NSURL(string: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4")!
+        player = AVPlayer(url: url as URL)
+        playerLayer = AVPlayerLayer(player: player)
+        playerLayer?.frame = view.bounds
+        view.layer.addSublayer(playerLayer!)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterBackground), name: UIApplication.didEnterBackgroundNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleEnterForeground), name: UIApplication.willEnterForegroundNotification, object: nil)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(playVideoFullScreen))
+        view.addGestureRecognizer(tapGesture)
+    }
+    
+    private func setupAVAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("AVAudioSession configuration failed: \(error)")
+        }
+    }
+    
+    @objc private func playVideoFullScreen() {
+        let playerViewController = AVPlayerViewController()
+        playerViewController.player = player
+        self.playerViewController = playerViewController
+        
+        present(playerViewController, animated: true) {
+            playerViewController.player?.play()
+        }
+    }
+    
+    @objc private func handleEnterBackground() {
+        playerViewController?.player = nil
+        playerLayer?.player = nil
+    }
+    @objc private func handleEnterForeground() {
+        playerViewController?.player = player
+        playerLayer?.player = player
+    }
 }
 
